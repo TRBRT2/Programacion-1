9886a8c2b1e08fc301c8f4349275083ec844b2f1
TurboRata <84026734+TRBRT2@users.noreply.github.com>
2021-10-20 17:17:54 -0400

    Create Asignacion 1.cs

---
+++ Asignacion 1.cs
@@ -0,0 +1,31 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		
+		Console.Write(a);
+		Console.Write(a);
+		Console.Write(c);
+		Console.WriteLine(b);
+		
+		
+		Console.Write(b);
+		Console.Write(c);
+		Console.WriteLine(a);
+		
+
+		Console.Write(b);
+		Console.Write(c);
+		Console.Write(b);
+		Console.WriteLine(c);
+		
+		
+		
+	}
+}
+
