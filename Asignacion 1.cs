9d2508dce3d67e0a803851a781dfe6dba8259b14
TurboRata <84026734+TRBRT2@users.noreply.github.com>
2021-10-20 18:03:22 -0400

    Update Asignacion 1.cs

--- Asignacion 1.cs
+++ Asignacion 1.cs
@@ -4,26 +4,29 @@ public class Program
 {
 	public static void Main()
 	{
-		int a = 1;
-		int b = 2;
-		int c = 3;
-		
+		int a = 0;
+		int b = 20;
+		int c = 10;
+        Console.WriteLine("--------");
+	    Console.WriteLine("Combinacion aacb ");
 		Console.Write(a);
 		Console.Write(a);
 		Console.Write(c);
 		Console.WriteLine(b);
-		
-		
+        Console.WriteLine(".......");
+
+	    Console.WriteLine("Combinacion bca ");
 		Console.Write(b);
 		Console.Write(c);
 		Console.WriteLine(a);
-		
+        Console.WriteLine(".......");
 
+        Console.WriteLine("Combinacion bcbc ");
 		Console.Write(b);
 		Console.Write(c);
 		Console.Write(b);
 		Console.WriteLine(c);
-		
+        Console.WriteLine("--------");
 		
 		
 	}
